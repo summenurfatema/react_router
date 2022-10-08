ad584aeac626a3606b3ac3e1a96a91673c53b212
Fatema04 <shadrinmoni15@gmail.com>
2022-10-08 11:13:27 -0700

    cart added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            order summary
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
 
 const Home = () => {
     const tshirts = useLoaderData()
     return (
-        <div>
-            {
-                tshirts.map(tshirt => <TShirt
-                    key={tshirt.id}
-                    tshirt={tshirt}
-                ></TShirt>)
-            }
+        <div className='home-container'>
+            <div className='tshirt-container'>
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt.id}
+                        tshirt={tshirt}
+                    ></TShirt>)
+                }
+            </div>
+
+            <div><Cart></Cart> </div>
+
         </div>
     );
 };
